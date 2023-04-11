74b12d88943efba9b8282e82ab0590b57e264107
Arkan <arkankhudiyev@gmail.com>
2023-04-12 01:50:55 +0400

    Js-task

---
+++ script.js
@@ -0,0 +1,46 @@
+$(function () {
+
+
+    $('img').attr('title', 'T-shirts')
+
+    $('#bg').css({
+        'width': '100%',
+        'height': '100%',
+        'background-repeat': 'no-repeat',
+        'background-size': 'contain',
+        'background-position': 'center',
+        'background-color': 'rgba(0,0,0,0.6)',
+        position: 'fixed',
+        top: 0,
+        display: 'none',
+
+    })
+
+    $('div.globalImg >img').click(function () {
+        let imgPath = $(this).attr('src')
+        $('#bg').css({
+            'background-image': `url('${imgPath}')`,
+            'display': 'block'
+        })
+
+    })
+   
+    $('#bg').click(function(){
+        $('#bg').css({
+            'display':'none'
+        })
+    })
+    
+    $('div.t_Shirts >img').click(function () {
+      
+        let imgPath = $(this).attr('src')
+
+        $('globalImg >img').attr('src',imgPath);
+       
+       
+       
+       
+    })
+
+
+})
